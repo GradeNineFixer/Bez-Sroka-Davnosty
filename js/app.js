b942b733c707771bead6e8b27b93b9d4471bb58e
GradeNineFixer <mmorrovich@gmail.com>
2025-04-13 20:31:27 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,13 +14,6 @@ const sliderMain = new Swiper('.slider_main', {
 		}
 	}
 })
-const sliderBg = new Swiper('.slider_bg', {
-	centeredSlides: true,
-	parallax: true,
-	spaceBetween: 60,
-	slidesPerView: 3.5
-})
-sliderMain.controller.control = sliderBg
 
 document.querySelectorAll('.slider__item').forEach(item => {
 	item.addEventListener('click', event => {
