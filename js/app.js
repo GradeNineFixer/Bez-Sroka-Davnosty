59a3c148a20b0ef2db8671777236b8370ca13b9b
GradeNineFixer <mmorrovich@gmail.com>
2025-04-13 19:58:55 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,16 @@
-const sliderMain = new Swiper('.slider_main', {
-	centeredSlides: true,
-	mousewheel: true,
-	parallax: true,
-	breakpoints: {
-		0: {
-			slidesPerView: 2.5,
-			spaceBetween: 20
-		},
-		680: {
-			slidesPerView: 3.5,
-			spaceBetween: 60
-		}
-	}
-})
+const swiper = new Swiper('.slider-main', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-/*Удалите этот код:
-const sliderBg = new Swiper('.slider_bg', {
-	centeredSlides: true,
-	parallax: true,
-	spaceBetween: 60,
-	slidesPerView: 3.5
-})
+    // Navigation arrows (по желанию)
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-sliderMain.controller.control = sliderBg
-*/
-
-document.querySelectorAll('.slider__item').forEach(item => {
-	item.addEventListener('click', event => {
-		item.classList.toggle('opened')
-	})
-})
-
-let desc = document.querySelector('.description')
-sliderMain.on('slideChange', e => {
-	sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
-})
+    // And if we need scrollbar (по желанию)
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
